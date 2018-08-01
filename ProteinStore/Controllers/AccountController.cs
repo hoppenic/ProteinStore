1d72fee05fd7b57583937cf85e59bf88274cb2a1
hoppenic <hoppenic@gmail.com>
2018-08-01 08:37:20 -0500

    update

---
+++ ProteinStore/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProteinStore.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace ProteinStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //htttpGET
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Come back here and make it so we log user in
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ ProteinStore/Models/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProteinStore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MinLength(5,ErrorMessage ="Must be at least 5 characters")]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(8,ErrorMessage ="Must be at least 8 characters")]
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+
+    }
+}
